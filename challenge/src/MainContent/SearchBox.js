e97451f71393a4e26f46bb44107fccefafb09fac
Ahkeem Lang <langahkeem@gmail.com>
2019-08-20 21:40:38 -0500

    cleaning up the styling

--- challenge/src/MainContent/SearchBox.js
+++ challenge/src/MainContent/SearchBox.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchBox = props =>
+<div className="searchBar">
 <input type="text"
+  className="searchBarPadding"
   value={props.search}
   onChange={props.updateSearch}
-  placeholder="Select documents..." />;
+  placeholder="Select documents..." />
+  </div>
 
 SearchBox.propTypes = {
   search: PropTypes.string,
