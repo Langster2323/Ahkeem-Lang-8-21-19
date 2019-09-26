c375c6b43d748b59ac318403fae3a6a7c9d208e0
Ahkeem Lang <langahkeem@gmail.com>
2019-09-25 20:14:48 -0500

    Fixed indent. Deleted loader test

--- challenge/src/MainContent/SearchBox.js
+++ challenge/src/MainContent/SearchBox.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SearchBox = props =>
 <div className="searchBar">
-<input type="text"
-  className="searchBarPadding"
-  value={props.search}
-  onChange={props.updateSearch}
-  placeholder="Select documents..." />
+  <input type="text"
+    className="searchBarPadding"
+    value={props.search}
+    onChange={props.updateSearch}
+    placeholder="Select documents..." />
   </div>
 
 export default SearchBox;
